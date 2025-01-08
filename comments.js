21cc0e6ec2203a2bc1a495084d909981b90416c7
sachinbachu <77490736+sachinbachu@users.noreply.github.com>
2025-01-08 11:22:12 +0000

    copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// create web server with express
+const express = require('express');
+const app = express();
+
+// create comments array
+const comments = [
+  { username: 'Todd', comment: 'lol so funny' },
+    { username: 'Skyler', comment: 'I like turtles' },
+    { username: 'Sk8erBoi', comment: 'Plz delete this post' },
+    { username: 'Ada', comment: 'that is so last year' }
+];
