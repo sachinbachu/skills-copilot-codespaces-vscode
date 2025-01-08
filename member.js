9cb6e8f32fa571452c86fc920308488404ac157f
sachinbachu <77490736+sachinbachu@users.noreply.github.com>
2025-01-08 11:18:16 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember()  {
+    var member = {
+        name: "John Doe",
+        age: 24,
+        skills: ["HTML", "CSS", "JS"],
+        greet: function() {
+            console.log("Hi there!");
+        }
+    };
+
+    return member;
+}
